cb203f48722e86978d4d9fd4feda52c6205f9f8b
ChintanPatel22 <165757443+ChintanPatel22@users.noreply.github.com>
2026-03-12 18:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,12 @@ def get_stats():
 def handle_connect():
     emit('connected', {'status': 'ok'})
 
+#if __name__ == '__main__':
+  #  os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+  #  os.makedirs(app.config['PRINTED_FOLDER'], exist_ok=True)
+  #  socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+
+
 if __name__ == '__main__':
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    os.makedirs(app.config['PRINTED_FOLDER'], exist_ok=True)
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
